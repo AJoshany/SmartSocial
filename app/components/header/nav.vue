13a8b169d38a5f6e7ef6b2b9bd4a51b2cbdb0d3d
Ali Joshany <aa6joshany@gmail.com>
2025-11-12 16:22:04 +0330

    init: add static header nav

---
+++ app/components/header/nav.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <NuxtLink v-for="(item,index) in navItems" :key="index" :to="item.path">
+      {{ item.title }}
+    </NuxtLink>
+  </nav>
+</template>
+
+<script setup>
+  const navItems = [
+    {title:'امکانات' , path:'/'},
+    {title:'تعرفه ها' , path:'/'},
+    {title:'بلاگ' , path:'/'},
+    {title:'تماس با ما' , path:'/'},
+  ]
+</script>
+
+<style>
+
+</style>
