620155a35413932a501af7b2130a97422e88c9c5
Ali.Joshany <aa6joshany@gmail.com>
2025-11-12 11:36:40 +0330

    chore: init

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="flex flex-col items-center">
+    <div class="container">
+      <slot></slot>
+    </div>
+  </main>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="font-bold text-amber-600">سلام بر دنیای برنامه نویسی</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
